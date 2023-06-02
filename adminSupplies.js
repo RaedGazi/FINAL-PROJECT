39e8013fa188c51544fef000becaae8d4187d024
Tiffany P <tvertican@gmail.com>
2023-06-02 17:48:24 -0400

    update adminSupplies.js

--- adminSupplies.js
+++ adminSupplies.js
@@ -8,17 +8,17 @@ const displayProd = function () {
     prodCards.innerHTML = ""
     prodArr.forEach(prod => {
         let card = document.createElement("div");
-        card.classList.add("", "")
+        card.classList.add("card", "card-body")
         card.innerHTML =
             `<div class="card" style="width: 18rem;">
-        <div class="card-body">
-         <h5 class="card-title">${prod.supplier}</h5>
-         <p class="card-text">${prod.name}</p>
+        <div class="card">
+         <img src="${prod.photo}" class="card-img-top" alt="Card image>
+         <div class="card-body">
+         <h5 class="card-title">${prod.name}</p>
          <p class="card-text">${prod.description}</p>
-         <p class="card-text">${prod.photo}</p>
-          <p class="card-text">${prod.quantity}</p>
          <p class="card-text">${prod.price}</p>
-         <a href="https://www.amazon.com/" class="btn btn-primary" target="_blank">Add to Cart</a>
+          <p class="card-text">${prod.quantity}</p>
+          <a href="#" class="btn btn-primary">Add to your cart</a>
          </div>
         </div>`
 
@@ -27,3 +27,17 @@ const displayProd = function () {
     console.log(prodArr)
 }
 displayProd()
+
+{/* <div class="card" style="width: 18rem;">
+        <img src="https://images.ciamedical.com/image/data/manual_import/LdYdg3mu68gTogRU-320x320.jpg"
+          class="card-img-top" alt="Standard Gown">
+        <div class="card-body">
+          <h5 class="card-title">Trimax Astound Standard Gown</h5>
+          <p class="card-text">Inspired by long days on your feet, our surgical gowns stand up to the rigors of your
+            work,
+            helping to keep you safe from potentially harmful elements. In creating Astound Gown, we worked closely with
+            healthcare professionals like you to make sure your needs and expectations were not just met, but exceeded.
+          </p>
+          <a href="#" class="btn btn-primary">Add to your cart</a>
+        </div>
+      </div> */}
