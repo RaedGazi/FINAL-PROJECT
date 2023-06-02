2674be81af9df66d997ad74e8c7c863657b7c2eb
Tiffany P <tvertican@gmail.com>
2023-06-02 15:24:16 -0400

    local storage

---
+++ admin.js
@@ -0,0 +1,56 @@
+const adminForm = document.getElementById("adminForm");
+const prodRows = document.getElementById("prodRows");
+
+const prodController = new ProdController()
+//test:console.log(prodController)
+
+//helper to display product on local storage
+//needs most recent version so use getLocalStorage
+const displayProd = function () {
+ //getLocalStorage will change to fetch once we add a database
+  let prodArr = prodController.getLocalStorage()
+ //the empty string below wipes what's there before adding in a whole new list again
+ prodRows.innerHTML = ""  
+
+prodArr.forEach(prod => {
+let newRow = document.createElement("tr");
+newRow.setAttribute("data-id", prod.id)
+  newRow.innerHTML =
+  `<td>${prod.supplier}</td>
+  <td>${prod.name}</td>
+  <td>${prod.description}</td>
+  <td>${prod.photo}</td>
+  <td>${prod.quantity}
+  <td>${prod.price}</td>`
+  
+  prodRows.append(newRow);
+})
+console.log(prodArr)
+}
+  //const addProdToTable = function (prodArr) {
+    //  const prodTable = document.getElementById("product-table");
+    
+    //addProdToTable(incomingData); 
+    
+    adminForm.addEventListener("submit", function (event) {
+      event.preventDefault()
+      //test: console.log("hello")
+      
+      const prodSupplier = document.getElementById("prodSupplier").value;
+      const prodName = document.getElementById("prodName").value;
+      const prodDescription = document.getElementById("prodDescription").value;
+      const prodPhoto = document.getElementById("prodPhoto").value;
+      const prodQuantity = document.getElementById("prodQuantity").value;
+      const prodPrice = document.getElementById("prodPrice").value;
+      //test:console.log(prodSupplier, prodName, prodDescription, prodPhoto, prodQuantity, prodPrice)
+      
+      //add product which creates an ojbect and pushes into prodArr so we see that object in the Array
+      //shows id going up by 1 in the console
+      prodController.addProduct(prodSupplier, prodName, prodDescription, prodPhoto, prodQuantity, prodPrice)
+      prodController.setLocalStorage()
+      console.log(prodController.prodArr)
+      displayProd()
+      adminForm.reset();
+    });
+  
+    displayProd()
---
+++ adminProductController.js
@@ -0,0 +1,46 @@
+//need this to set product, and have an object to work with that has all the logic for your product
+class ProdController {
+    constructor() {
+        /*currentID is because as you add new product 
+        you need a unique id for each product, it's the
+         one piece of data that only exists for that 
+         item. local storage doesn't create Ids so we 
+         need to make one for our product*/
+        this.currentID = 0
+        //this array gets stored in local storage as key-value pairs
+        this.prodArray = []
+    }
+    //assign ID to product and put in the array so need actions:
+    //create object and add to array, takes in your event listener from admin.js
+
+    addProduct(supplier, name, description, photo, quantity, price) {
+        const prodObj = {
+            supplier,
+            name,
+            description,
+            photo,
+            quantity,
+            price,
+            //so we can add more products we use id:... to have the ID increase by1 because local storage doesn't create IDs:
+            id: this.currentID++
+        }
+        this.prodArray.push(prodObj)
+    }
+
+    //control setting and getting item
+    setLocalStorage() {
+        //do || [] instead of using an if statement to show to use what's in local storage and if there's nothing in there return empty array
+        localStorage.setItem("product", JSON.stringify(this.prodArray)) 
+        //currentID is just for local storage, you won't need it when you have a database linked, you need it to update and delete
+        localStorage.setItem("current-id", JSON.stringify(this.currentID)) 
+    }
+    //getItem takes in the key and returns current value or null if it doesn't exist
+    //to get something out and do something with it, we PARSE what's in local storage
+    getLocalStorage() {
+        this.prodArray = JSON.parse(localStorage.getItem("product")) || []
+        this.currentID = JSON.parse(localStorage.getItem("current-id")) || 0
+        return this.prodArray
+    }
+}
+
+//clear local storage by hitting the circle with the line through it
