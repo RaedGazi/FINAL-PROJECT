10a77bbd65a630623cf25df63185406686e29272
Tiffany P <tvertican@gmail.com>
2023-07-17 13:20:45 -0400

    added api

--- admin.js
+++ admin.js
@@ -7,48 +7,48 @@ const prodController = new ProdController()
 
 //helper to display product on local storage
 //needs most recent version so use getLocalStorage
-const displayProd = function () {
-  //getLocalStorage will change to fetch once we add a database
-  let prodArr = prodController.getLocalStorage()
-  //the empty string below wipes what's there before adding in a whole new list again
-  prodRows.innerHTML = ""
-  prodArr.forEach(prod => {
-    let newRow = document.createElement("tr");
-    newRow.setAttribute("data-id", prod.id)
-    newRow.innerHTML =
-      `<td><img src="${prod.photo}"></td>
-  <td>${prod.name}</td>
-  <td>${prod.description}</td>
-  <td>${prod.price}</td>
-  <td>${prod.quantity}</td>`
+const displayProd = async function () {
+ //getLocalStorage will change to fetch once we add a database
+ let prodArr = await prodController.getAllProducts()
+ //the empty string below wipes what's there before adding in a whole new list again
+ prodRows.innerHTML = ""
+ prodArr.forEach(prod => {
+   let newRow = document.createElement("tr");
+   newRow.setAttribute("data-id", prod.id)
+   newRow.innerHTML =
+     `<td><img src="${prod.url}"></td>
+ <td>${prod.name}</td>
+ <td>${prod.description}</td>
+ <td>${prod.price}</td>
+ <td>${prod.quantity}</td>`
 
-    prodRows.append(newRow);
-  })
-  console.log(prodArr)
+   prodRows.append(newRow);
+ })
+ console.log(prodArr)
 }
 //const addProdToTable = function (prodArr) {
 //  const prodTable = document.getElementById("product-table");
 
-//addProdToTable(incomingData); 
+//addProdToTable(incomingData);
 
-adminForm.addEventListener("submit", function (event) {
-  event.preventDefault()
-  //test: console.log("hello")
+adminForm.addEventListener("submit", async function (event) {
+ event.preventDefault()
+ //test: console.log("hello")
 
-  const prodPhoto = document.getElementById("prodPhoto").value;
-  const prodName = document.getElementById("prodName").value;
-  const prodDescription = document.getElementById("prodDescription").value;
-  const prodPrice = document.getElementById("prodPrice").value;
-  const prodQuantity = document.getElementById("prodQuantity").value;
-  //test:console.log(prodPhoto, prodName, prodDescription, prodPrice, prodQuantity)
+ const prodPhoto = document.getElementById("prodPhoto").value;
+ const prodName = document.getElementById("prodName").value;
+ const prodDescription = document.getElementById("prodDescription").value;
+ const prodPrice = document.getElementById("prodPrice").value;
+ const prodQuantity = document.getElementById("prodQuantity").value;
+ //test:console.log(prodPhoto, prodName, prodDescription, prodPrice, prodQuantity)
 
-  //add product which creates an ojbect and pushes into prodArr so we see that object in the Array
-  //shows id going up by 1 in the console
-  prodController.addProduct(prodPhoto, prodName, prodDescription, prodPrice, prodQuantity)
-  prodController.setLocalStorage()
-  //console.log(prodController.prodArr)
-  displayProd()
-  adminForm.reset();
+ //add product which creates an ojbect and pushes into prodArr so we see that object in the Array
+ //shows id going up by 1 in the console
+await prodController.createSupply(prodPhoto, prodName, prodDescription, prodPrice, prodQuantity)
+ //prodController.setLocalStorage()
+ //console.log(prodController.prodArr)
+await displayProd()
+ adminForm.reset();
 });
 
 displayProd()
